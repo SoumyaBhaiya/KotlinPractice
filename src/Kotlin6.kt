ca35b3acb66472f95f3204de71cf291db938a48d
Soumya Bhaiya <soumybhaiya@gmail.com>
2025-08-05 15:26:28 -0700

    Added more functions and methods

--- src/Kotlin6.kt
+++ src/Kotlin6.kt
@@ -1,17 +1,21 @@
-import jdk.jfr.Category
-
 fun main(){
+    var smartDevice: SmartDevice = SmartTvDevice("Android TV", "Entertainment")
+    smartDevice.turnOn()
 
+    smartDevice = SmartLightDevice("Google Light", "Utility")
+    smartDevice.turnOn()
 }
 
 
 
 open class SmartDevice(val name: String, val category: String){
-    fun turnOn(){
+    var deviceStatus = "online"
+
+    open fun turnOn(){
         println("Smart device has turned on")
     }
 
-    fun turnOff(){
+    open fun turnOff(){
         println("Smart device has turned off")
     }
 }
@@ -40,6 +44,18 @@ class SmartTvDevice(deviceName: String, deviceCategory: String): SmartDevice(nam
         channelNumber++
         println("Channel changed to $channelNumber")
     }
+    override fun turnOn() {
+        deviceStatus = "on"
+        println(
+            "$name is turned on. Speaker volume is set to $speakerVolume and channel number is " +
+                    "set to $channelNumber."
+        )
+    }
+
+    override fun turnOff() {
+        deviceStatus = "off"
+        println("$name turned off")
+    }
 }
 
 class SmartLightDevice(deviceName: String, deviceCategory: String) :
@@ -56,13 +72,47 @@ class SmartLightDevice(deviceName: String, deviceCategory: String) :
         brightnessLevel++
         println("Brightness increased to $brightnessLevel.")
     }
+
+    override fun turnOn(){
+        deviceStatus = "on"
+        brightnessLevel = 2
+        println("$name turned on. The brightness level is at $brightnessLevel")
+    }
+
+    override fun turnOff(){
+        deviceStatus = "off"
+        brightnessLevel = 0
+        println("Smart Light turned off")
+    }
 }
 
-class SmartHome(val smartTvDevice: SmartTvDevice){
+class SmartHome(val smartTvDevice: SmartTvDevice, val smartLightDevice: SmartLightDevice){
     fun turnOnTv(){
         smartTvDevice.turnOn()
     }
     fun turnOffTv(){
         smartTvDevice.turnOff()
+    }
+    fun increaseTvVolume(){
+        smartTvDevice.increaseSpeakerVolume()
+    }
+
+    fun changeTvChannelToNext(){
+        smartTvDevice.nextChannel()
+    }
+    fun turnOnLight() {
+        smartLightDevice.turnOn()
+    }
+
+    fun turnOffLight() {
+        smartLightDevice.turnOff()
+    }
+    fun increaseLightBrightness() {
+        smartLightDevice.increaseBrightness()
+    }
+
+    fun turnOffAllDevices() {
+        turnOffTv()
+        turnOffLight()
     }
 }
