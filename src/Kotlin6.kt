73b7a82db9d54b86ff4a70e4be8daeaaa2456b9f
Soumya Bhaiya <soumybhaiya@gmail.com>
2025-08-07 13:04:38 -0700

    Working with interfaces.

--- src/Kotlin6.kt
+++ src/Kotlin6.kt
@@ -1,3 +1,23 @@
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+class RangeRegulator(
+    initialValue: Int,
+    private val minValue: Int,
+    private val maxValue: Int
+) : ReadWriteProperty <Any?, Int>{
+    var fieldData = initialValue
+    override fun getValue(thisRef: Any?, property: KProperty<*>):Int{
+        return fieldData
+    }
+
+    override fun setValue(thisRef:Any?, property: KProperty<*>, value:Int){
+        if (value in minValue..maxValue){
+            fieldData = value
+        }
+    }
+}
+
 fun main(){
     var smartDevice: SmartDevice = SmartTvDevice("Android TV", "Entertainment")
     smartDevice.turnOn()
